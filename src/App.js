1fe48014c23d84c85141c8d8b50d1c071d76721a
Ujjawal1640 github <ujjawal1696251@gmail.com>
2023-05-23 13:16:34 +0530

    small addition

--- src/App.js
+++ src/App.js
@@ -153,7 +153,7 @@ const App = () => {
       
       </VStack>
 <VStack p={'5'}>
-      <Heading>Add a Contact</Heading>
+      <Heading>Add a Book</Heading>
       <form onSubmit={handleAddFormSubmit}>
       <VStack my={"6"}   w={"100%"}
     maxWidth={{base:'90vw',sm:'80vw',lg:'50vw',xl:'40vw'}} >
